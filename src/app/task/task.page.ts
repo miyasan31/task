ebb2f9137f60f64a8a6c8b92ccf073c9ec51bb20
ShotaMiyahara <smiyahara@landscape.co.jp>
2022-05-10 15:23:17 +0900

    ✨ タスクページ追加

---
+++ src/app/task/task.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.page.html',
+  styleUrls: ['./task.page.scss'],
+})
+export class TaskPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
