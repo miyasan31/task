70eec232d26386a03717baa5a38bbbcb3556f611
miyasan_0301 <miyasan.19990301@gmail.com>
2022-05-11 09:32:57 +0900

    ✨ DI: ComponentsModule

--- src/app/timeline/timeline.module.ts
+++ src/app/timeline/timeline.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
-import { TimelinePageRoutingModule } from './timeline-routing.module';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ExploreContainerComponentModule } from '~/explore-container/explore-container.module';
+import { ComponentsModule } from '~/components/components.module';
 
 import { TimelinePage } from './timeline.page';
+import { TimelinePageRoutingModule } from './timeline-routing.module';
 
 @NgModule({
   imports: [
@@ -16,6 +16,7 @@ import { TimelinePage } from './timeline.page';
     IonicModule,
     TimelinePageRoutingModule,
     ExploreContainerComponentModule,
+    ComponentsModule,
   ],
   declarations: [TimelinePage],
 })
