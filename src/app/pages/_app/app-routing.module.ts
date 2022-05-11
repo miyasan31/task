19b7f9688030134717804c867f659adb55a22d52
miyasan_0301 <miyasan.19990301@gmail.com>
2022-05-12 01:46:12 +0900

    ✨ AuthGurdでリダイレクト処理追加

--- src/app/pages/_app/app-routing.module.ts
+++ src/app/pages/_app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+// 認証情報がない場合はサインイン画面にリダイレクト
+const redirectUnauthorized = () => redirectUnauthorizedTo(['signin']);
+// 認証情報がある場合はアプリ画面にリダイレクト
+const redirectLoggedIn = () => redirectLoggedInTo(['/task']);
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('~/pages/tabs/tabs.module').then((m) => m.TabsPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorized },
   },
   {
     path: 'signin',
     loadChildren: () => import('~/pages/signin/signin.module').then((m) => m.SigninPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedIn },
   },
 ];
 @NgModule({
