9aa021a1d1b0965c100127f606abfb4554acffb2
miyasan_0301 <miyasan.19990301@gmail.com>
2022-05-12 02:35:20 +0900

    ✨ Googleサインイン追加

--- src/app/pages/signin/signin.page.ts
+++ src/app/pages/signin/signin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '~/services/auth/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
-  constructor() {}
+  constructor(public auth: AuthService) {}
 
   ngOnInit() {}
+
+  onGoogleSignIn() {
+    this.auth.googleSignIn();
+  }
 }
