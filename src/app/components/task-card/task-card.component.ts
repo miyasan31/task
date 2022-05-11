612d56d00c4efe237ac0e326d6635950da8d664d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-05-11 09:31:53 +0900

    ✨ タスクカード作成

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IUser } from '~/interface/IUser';
 
 @Component({
   selector: 'app-task-card',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent implements OnInit {
+  @Input() user: Pick<IUser, 'id' | 'user_name' | 'avatar'>;
+  @Input() updated_at: number;
+  @Input() task_list: string[];
+  @Input() is_like: boolean;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.info(this.task_list);
+  }
 }
