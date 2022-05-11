6775554422813c41a42cd1f34028447531538234
miyasan_0301 <miyasan.19990301@gmail.com>
2022-05-12 00:49:08 +0900

    🚚 ディレクトリ構成変更

--- src/app/interface/ILike.ts
+++
@@ -1,9 +0,0 @@
-import type { IUser } from './IUser';
-import type { ITask } from './ITask';
-
-export type ILike = {
-  id: number;
-  userId: IUser['id'];
-  taskId: ITask['id'];
-  createdAt: number;
-};
--- src/app/interface/IUser.ts
+++
@@ -1,9 +0,0 @@
-export type IUser = {
-  id: string;
-  userName: string;
-  email: string;
-  profile: string;
-  avatar: string;
-  createdAt: number;
-  updatedAt: number;
-};
