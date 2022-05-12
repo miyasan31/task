f51e14845bbb34c098a47b419e5cdd8fc1df3dfc
ShotaMiyahara <smiyahara@landscape.co.jp>
2022-05-12 11:41:38 +0900

    ✨ user service

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+import { IUser } from '~/interfaces/IUser';
+import { AuthService } from '~/services/auth/auth.service';
+import { UserRepository } from '~/repositories/user/user.repository';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(public auth: AuthService, public userRepository: UserRepository) {}
+
+  getUser(userId: IUser['id']): Promise<IUser> {
+    return this.userRepository.getUser(userId);
+  }
+
+  createUser(user: IUser): Promise<void> {
+    return this.userRepository.createUser(user);
+  }
+
+  updateUser(user: IUser): Promise<void> {
+    return this.userRepository.updateUser(user);
+  }
+
+  deleteUser(userId: IUser['id']): Promise<void> {
+    return this.userRepository.deleteUser(userId);
+  }
+}
