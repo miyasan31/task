027d89c819022a4882f7030114e2f0b7181f0aa0
ShotaMiyahara <smiyahara@landscape.co.jp>
2022-05-12 11:55:51 +0900

    🩹 バグ修正

--- src/app/repositories/user/user.repository.ts
+++ src/app/repositories/user/user.repository.ts
@@ -13,40 +13,39 @@ import { first } from 'rxjs/operators';
 
 import { IUser } from '~/interfaces/IUser';
 
-type UserDocument = DocumentReference<IUser>;
-type UserCollection = CollectionReference<IUser>;
+type UserDocRef = DocumentReference<IUser>;
+type UserColRef = CollectionReference<IUser>;
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserRepository {
-  userDoc: UserDocument;
-  userCol: UserCollection;
+  userDocRef: UserDocRef;
+  userColRef: UserColRef;
 
   constructor(public firestore: Firestore) {
-    this.userDoc = doc(this.firestore, 'users') as UserDocument;
-    this.userCol = collection(this.firestore, 'users') as UserCollection;
+    this.userColRef = collection(this.firestore, 'users') as UserColRef;
   }
 
   // ユーザー情報を取得する
   getUser(uerId: IUser['id']): Promise<IUser> {
-    const userDoc = doc(this.firestore, `users/${uerId}`) as UserDocument;
+    const userDoc = doc(this.firestore, `users/${uerId}`) as UserDocRef;
     return docData<IUser>(userDoc).pipe(first()).toPromise(Promise);
   }
 
   // ユーザー情報を保存する
   createUser(userDto: IUser): Promise<void> {
-    return setDoc(this.userDoc, userDto);
+    return setDoc(this.userDocRef, userDto);
   }
 
   // ユーザー情報を更新する
   updateUser(userDto: IUser): Promise<void> {
-    return setDoc(this.userDoc, userDto, { merge: true });
+    return setDoc(this.userDocRef, userDto, { merge: true });
   }
 
   // ユーザー情報を削除する
   deleteUser(userId: IUser['id']): Promise<void> {
-    const userDoc = doc(this.firestore, `users/${userId}`) as UserDocument;
+    const userDoc = doc(this.firestore, `users/${userId}`) as UserDocRef;
     return deleteDoc(userDoc);
   }
 }
