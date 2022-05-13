2a084139054e45aa98b0f7165cddc0be368d505a
ShotaMiyahara <smiyahara@landscape.co.jp>
2022-05-13 17:07:19 +0900

    💄 スタイル変更

--- src/app/pages/timeline-detail/timeline-detail.page.ts
+++ src/app/pages/timeline-detail/timeline-detail.page.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from '~/services/task/task.service';
 import { Observable } from 'rxjs';
 import { ITask } from '~/interfaces/ITask';
+import { IUser } from '~/interfaces/IUser';
+import { UserService } from '~/services/user/user.service';
 
 @Component({
   selector: 'app-timeline-detail',
@@ -12,13 +14,20 @@ import { ITask } from '~/interfaces/ITask';
 export class TimelineDetailPage implements OnInit {
   userId: string;
   taskList: Observable<ITask[]>;
+  user: IUser;
 
-  constructor(public route: ActivatedRoute, public taskService: TaskService) {}
+  constructor(
+    public route: ActivatedRoute,
+    public taskService: TaskService,
+    public userService: UserService,
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.route.paramMap.subscribe((prams: ParamMap) => {
       this.userId = prams.get('userId');
     });
+
+    this.user = await this.userService.getUser(this.userId);
     this.taskList = this.taskService.getTaskList(this.userId);
   }
 }
