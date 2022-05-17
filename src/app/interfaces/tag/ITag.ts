16b7f598048ce1f1d62c1580e846b23fc08ff5f9
ShotaMiyahara <smiyahara@landscape.co.jp>
2022-05-17 15:32:13 +0900

    🏷️ colorの型をリテラルに変更

--- src/app/interfaces/tag/ITag.ts
+++ src/app/interfaces/tag/ITag.ts
@@ -1,10 +1,13 @@
 import { Timestamp } from '@angular/fire/firestore';
+import { colorPallet } from '~/constants/colorPicker';
 import { IUser } from '~/interfaces/user/IUser';
+
+type ColorScheme = typeof colorPallet[number]['name'];
 
 export interface ITag {
   id: string;
   tagName: string;
-  color: string;
+  color: ColorScheme;
   isActive: boolean;
   userId: IUser['id'];
   createdAt?: Timestamp;
