1f370236d480c036437cf05c8a66a43f066ebabb
ShotaMiyahara <smiyahara@landscape.co.jp>
2022-05-19 18:32:17 +0900

    🐛 RouterModule import忘れ

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { TaskCardComponent } from './task-card/task-card.component';
@@ -9,6 +10,6 @@ import { TaskDetailCardComponent } from './task-detail-card/task-detail-card.com
 @NgModule({
   declarations: [TaskCardComponent, TaskDetailCardComponent],
   exports: [TaskCardComponent, TaskDetailCardComponent],
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
 })
 export class ComponentsModule {}
