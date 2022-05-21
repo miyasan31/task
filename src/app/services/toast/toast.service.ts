8303687578928451f6d0feb32b0c82b151982b95
miyasan_0301 <miyasan.19990301@gmail.com>
2022-05-21 20:18:39 +0900

    🔥presentToastWithOptions()

--- src/app/services/toast/toast.service.ts
+++ src/app/services/toast/toast.service.ts
@@ -16,35 +16,4 @@ export class ToastService {
 
     toast.present();
   }
-
-  private async presentToastWithOptions() {
-    const toast = await this.toastController.create({
-      header: 'Toast header',
-      message: 'Click to Close',
-      icon: 'information-circle',
-      position: 'top',
-      buttons: [
-        {
-          side: 'start',
-          icon: 'star',
-          text: 'Favorite',
-          handler: () => {
-            console.log('Favorite clicked');
-          },
-        },
-        {
-          text: 'Done',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          },
-        },
-      ],
-    });
-
-    await toast.present();
-
-    const { role } = await toast.onDidDismiss();
-    console.log('onDidDismiss resolved with role', role);
-  }
 }
