fc5dd3d174ddf5cc899d01aec25bc66d12077cca
miyasan_0301 <miyasan.19990301@gmail.com>
2022-05-23 09:35:36 +0900

    💄 tagカラーをion-badgeに反映

--- src/app/constants/colorPicker.ts
+++ src/app/constants/colorPicker.ts
@@ -1,15 +1,15 @@
 export const colorPallet = [
   { id: 0, name: '', color: '#000' },
-  { id: 1, name: 'pink', color: '#F3C6E2' },
-  { id: 2, name: 'tomato', color: '#FAC7BE' },
-  { id: 3, name: 'orange', color: '#FFCCA7' },
-  { id: 4, name: 'yellow', color: '#F9E68C' },
-  { id: 5, name: 'green', color: '#B4DFC4' },
-  { id: 6, name: 'mint', color: '#A5E4D4' },
-  { id: 7, name: 'sky', color: '#A4DFF1' },
-  { id: 8, name: 'blue', color: '#B7D9F8' },
-  { id: 9, name: 'violet', color: '#D7CFF9' },
-  { id: 10, name: 'gray', color: '#DFE3E6' },
+  { id: 1, name: 'pink', color: '#E93D82' },
+  { id: 2, name: 'tomato', color: '#E54D2E' },
+  { id: 3, name: 'orange', color: '#F76808' },
+  { id: 4, name: 'yellow', color: '#FFB224' },
+  { id: 5, name: 'green', color: '#30A46C' },
+  { id: 6, name: 'mint', color: '#70E1C8' },
+  { id: 7, name: 'sky', color: '#68DDFD' },
+  { id: 8, name: 'blue', color: '#0091FF' },
+  { id: 9, name: 'violet', color: '#6E56CF' },
+  { id: 10, name: 'gray', color: '#889096' },
 ] as const;
 
 export const colorPicker = [
